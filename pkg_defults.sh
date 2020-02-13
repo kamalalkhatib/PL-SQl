581e40b8cebb5d1428c349cb847193e1c4eb6d55
Kamal Alkhatib <alkhatib_kamal@yahoo.com>
2020-02-13 04:31:17 -0500

    Update pkg_defults.sh

--- pkg_defults.sh
+++ pkg_defults.sh
@@ -1,9 +1,10 @@
--- This package shows how we can define values for constants 
--- then we can use them in different equations   
+-- This package shows how we can define values for constants and equations
+-- then we can use them in different places   
 create or replace package default_values_pkg
                  as
                  function basic_hrs_fun return number;
-         function over_time_sal return number;
+	         function over_time_sal return number;
+		 function sal_calc_equation(p_em_w_hr number,p_pos_sal_hr number) return number;
                 end;
 create or replace package body default_values_pkg
                  is
@@ -19,4 +20,17 @@ create or replace package body default_values_pkg
                  begin
                  return v_over_time_sal;
                  end;
-                 end default_values_pkg;
+		function sal_calc_equation(p_em_w_hr number,p_pos_sal_hr number) return number
+		is
+		v_ov_tm_sal emp_sal.net_sal%type;--over time sal
+	        v_tot_tm_emp_sal.net_sal%type;--total time sal
+		begin
+            	  if v_em_w_hr>v_basic_hrs then
+ 	   	    v_ov_tm_sal:=(v_em_w_hr-v_basic_hrs)*(v_pos_sal_hr*v_over_time_sal);
+ 	   	    v_tot_tm_sal:=(v_basic_hrs*v_pos_sal_hr)+v_ov_tm_sal;
+ 	   	else
+ 	   	   v_tot_tm_sal:=(v_em_w_hr*v_pos_sal_hr);
+ 	   	end if;
+ 	   	return v_tot_tm_sal;
+ 	   	end;
+                end default_values_pkg;
